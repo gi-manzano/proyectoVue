3cb2d8822e844c43ff5a53ce12b40c7b481fbcca
gi-manzano <manzanogisele85@gmail.com>
2022-07-20 20:23:15 +0100

    proyectoVue 1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,97 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div v-if="!canAccess">
+      <ul class="nav nav-pills nav-justified mb-3">
+        <li class="nav-item">
+          <a class="navbar-toggler active" data-mdb-toggle="pill" @click="cambiardeRutaLogin" role="tab" aria-controls="pills-login" aria-selected="true">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="navbar-toggler" data-mdb-toggle="pill" @click="cambiardeRutaLogin" role="tab" aria-controls="pills-register" aria-selected="false">Register</a>
+        </li>
+      </ul>
+
+      <login-page 
+      v-show="estoyEnLogin"
+      @changeFlag="recibiElMensaje" 
+      :usuarios="listadoDeUsuarios"/>
+    
+      <registro-page
+      v-show="!estoyEnLogin"
+      @enviarRegistro="recibirRegistro" 
+      />
+    </div>
+    
+    <div v-else>
+    <main-page 
+    v-if="canAccess" 
+    @changeFlagFromMain="recibiElMensaje" 
+    :productos="listadoDeProductos"
+    @emitVerDetalle="recibirVerDetalle"
+    @emitAgregarAlCarrito="recibirAgregarAlCarrito"
+    />
+    
+    <carrito-page :carro='carrito' @emitActualizarCarritoPrincipal="recibirActualizarAlCarrito"/>
+    
+    <detalle-page v-if="selected" :producto="selected"/>
+
+    </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginPage from './components/LoginPage.vue'
+import RegistroPage from './components/RegistroPage.vue'
+import MainPage from './components/MainPage.vue'
+import CarritoPage from './components/CarritoPage.vue'
+import DetallePage from './components/DetallePage.vue'
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainPage,
+    LoginPage,
+    RegistroPage,
+    CarritoPage,
+    DetallePage,
+    
+    
+  },
+  data(){
+    return{
+    canAccess:false,
+    estoyEnLogin:true,
+    listadoDeUsuarios:[],
+    listadoDeProductos:[
+  
+
+      
+     ],
+    carrito:[],
+    selected: null
+    }
+  },
+  methods:{
+    recibiElMensaje(){
+      this.canAccess = !this.canAccess
+    },
+    recibirRegistro(payload){
+      this.listadoDeUsuarios.push(payload)
+    },
+    recibirAgregarAlCarrito(payload){
+      this.carrito.push(payload)
+    },
+    recibirActualizarAlCarrito(payload){
+          this.carrito = [...payload]
+          //this.carrito = Object.assing(this.carrito,payload)
+    },
+    recibirVerDetalle(payload){
+      this.selected = payload
+    },
+    cambiardeRutaLogin(){
+      this.estoyEnLogin = !this.estoyEnLogin;
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import 'jquery'
+import '@popperjs/core'
+
+
 
 Vue.config.productionTip = false
 
